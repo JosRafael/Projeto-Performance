677709418fc7938d2a4a347a97ce07895477d332
JosRafael <rafaelmatiashue@gmail.com>
2024-08-05 11:09:44 -0300

    removendo as ref

--- meu-projeto-react/src/App.jsx
+++ meu-projeto-react/src/App.jsx
@@ -1,24 +1,18 @@
 import { useState, useRef } from 'react'
 import './App.css'
 import { Header } from './Header';
-
+import { useForm } from 'react-hook-form';
 
 function App() {
   const nameRef = useRef(null);
   const emailRef = useRef(null);
   const usernameRef = useRef(null);
-  const descriptionRef = useRef(null);
 
 
   function handleSave(e){
     e.preventDefault();
 
-    console.log({
-      name: nameRef.current?.value,
-      email: emailRef.current?.value,
-      username: usernameRef.current?.value,
-      description: descriptionRef.current?.value,
-    })
+ 
   }
 
 
@@ -35,29 +29,20 @@ function App() {
           type="text"
           placeholder="Digite seu nome..."
           className="input"
-          ref={nameRef}
         />
 
         <input
           type="text"
           placeholder="Digite seu email..."
           className="input"
-          ref={emailRef}
         />
 
         <input
           type="text"
           placeholder="Digite seu username..."
           className="input"
-          ref={usernameRef}
         />
 
-        <textarea
-          type="text"
-          placeholder="Digite sua descriçao..."
-          className="input"
-          ref={descriptionRef}
-        ></textarea>
 
         <button className="button" type="submit">Enviar</button>
       </form>
